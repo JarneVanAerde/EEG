99c5a82709106a206923183ac126201e92d69b49
Bryan <br.deridder@gmail.com>
2018-10-17 19:25:48 +0200

    Added basic JavaFX classes

---
+++ EEG/src/be/kdg/eeg/presentation/StartView.scala
@@ -0,0 +1,22 @@
+package be.kdg.eeg.presentation
+
+import javafx.scene.chart.{LineChart, NumberAxis}
+import javafx.scene.layout.{BorderPane, VBox}
+
+class StartView extends BorderPane {
+
+  val lineChart: LineChart[Number, Number] = {
+    val xAxis = new NumberAxis
+    xAxis.setLabel("No of employees")
+    val yAxis = new NumberAxis
+    yAxis.setLabel("Revenue per employee")
+    new LineChart(xAxis,yAxis)
+  }
+
+  layoutNodes(lineChart)
+
+  def layoutNodes(arg1: LineChart[Number, Number]): Unit = {
+    val vbox = new VBox(arg1)
+    this.setCenter(vbox)
+  }
+}
