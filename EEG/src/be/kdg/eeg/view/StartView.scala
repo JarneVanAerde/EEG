19139ce46ca7e55a42938c92f40aa68adccff6e7
Bryan <br.deridder@gmail.com>
2018-11-27 15:59:53 +0100

    updated graph layout

--- EEG/src/be/kdg/eeg/view/StartView.scala
+++ EEG/src/be/kdg/eeg/view/StartView.scala
@@ -2,7 +2,7 @@ package be.kdg.eeg.views
 
 import javafx.geometry.{Insets, Pos}
 import javafx.scene.chart.{LineChart, NumberAxis, XYChart}
-import javafx.scene.control.{ComboBox, Label}
+import javafx.scene.control.{Button, ComboBox, Label}
 import javafx.scene.layout.{BorderPane, HBox, VBox}
 
 class StartView extends BorderPane {
@@ -10,6 +10,7 @@ class StartView extends BorderPane {
   final val PADDING = new Insets(10, 10, 10, 10)
   val comboBoxStimulus = new ComboBox[String]
   val comboBoxContactPoint = new ComboBox[String]
+  val btnClear = new Button("Clear")
   val lineChart: LineChart[Number, Number] = {
     val xAxis = new NumberAxis
     xAxis.setLabel("Time")
@@ -19,18 +20,20 @@ class StartView extends BorderPane {
     val line = new LineChart(xAxis, yAxis)
     line
   }
-  layoutNodes(lineChart, comboBoxStimulus, comboBoxContactPoint)
 
-  def layoutNodes(chart: LineChart[Number, Number], comboBoxStimulus: ComboBox[String], comboBoxContactPoint: ComboBox[String]): Unit = {
+  layoutNodes(lineChart, comboBoxStimulus, comboBoxContactPoint, btnClear)
+
+  def layoutNodes(chart: LineChart[Number, Number], comboBoxStimulus: ComboBox[String],
+                  comboBoxContactPoint: ComboBox[String], btnClear: Button): Unit = {
     val vbox1 = new VBox(new Label("Stimulus:"), comboBoxStimulus)
     val vbox2 = new VBox(new Label("Contact point:"), comboBoxContactPoint)
-    val hbox = new HBox(vbox1, vbox2)
+    val hbox = new HBox(vbox1, vbox2, btnClear)
     hbox.setAlignment(Pos.CENTER)
     val comboboxPane = new BorderPane(hbox)
     vbox1.setPadding(PADDING)
     vbox2.setPadding(PADDING)
     chart.setCreateSymbols(false)
-    this.setTop(chart)
-    this.setCenter(comboboxPane)
+    this.setCenter(chart)
+    this.setBottom(comboboxPane)
   }
 }
