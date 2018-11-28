37406c7a97a858e7845a7a5f1e71f6ec190113f6
jarne <jarne@LAPTOP-40O2DF4T>
2018-11-28 08:25:45 +0100

    Added documentation

--- EEG/src/be/kdg/eeg/model/analysis/AnalysisTools.scala
+++ EEG/src/be/kdg/eeg/model/analysis/AnalysisTools.scala
@@ -38,10 +38,28 @@ class AnalysisTools(val stimulusService: StimulusService) {
     contactPoints.slice(minRangeSlice, maxRangeSlice).sum / contactPoints.length
   }
 
-  def getSlidingWindowAvgs(stimulusString: String, contactPointString: String, slidingWindowSize: Int, minRange: Int = 1, mexRange: Int = 4): Vector[Double] = {
+  /**
+    * Calculates sliding window averages for all the contact points from a given
+    * stimulus object.
+    *
+    * @param stimulusString The word of the stimulus.
+    * @param contactPointString The name of the contact point.
+    * @param slidingWindowSize the size of the sliding window, this will effect the average calculation.
+    * @return A vector of sliding window averages.
+    */
+  def getSlidingWindowAvgs(stimulusString: String, contactPointString: String, slidingWindowSize: Int = 3): Vector[Double] = {
     getSlidingWindowAvg(stimulusService.getContactPointValuesForStimulus(stimulusString, contactPointString), slidingWindowSize)
   }
 
+  /**
+    * Recursive function will calculate the average of each sliding window.
+    *
+    * @param points The Contact points to calculate
+    * @param size The size of the sliding window
+    * @param avgs An empty vector that will contain the averages of each sliding window
+    * @param counter Counter that will keep track of the sliding window steps
+    * @return A vector that contains the average calculations
+    */
   private def getSlidingWindowAvg(points: Vector[Double], size: Int, avgs: Vector[Double] = Vector[Double](), counter: Int = 0): Vector[Double] = {
     if (counter <= points.length - size) {
       val new_avgs = avgs :+ points.slice(counter, counter + size).sum / size
