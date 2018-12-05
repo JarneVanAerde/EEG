354761243281917263e2b4d79a1506731e0207cc
Bryan <br.deridder@gmail.com>
2018-12-05 23:12:35 +0100

    added animations

--- EEG/src/be/kdg/eeg/presenter/MenuPresenter.scala
+++ EEG/src/be/kdg/eeg/presenter/MenuPresenter.scala
@@ -1,12 +1,21 @@
 package be.kdg.eeg.presenter
 
 import be.kdg.eeg.view.{MenuView, RegularChartView, SlidingWindowView}
+import javafx.animation.ScaleTransition
+import javafx.scene.control.Button
+import javafx.util.Duration
 
 class MenuPresenter(val view: MenuView) {
   addEventHandlers()
-  updateView()
 
   def addEventHandlers(): Unit = {
+    Array(
+      view.btnSlidingWindow,
+      view.btnRegularChart
+    ).foreach(button => {
+      button.setOnMouseEntered((event) => scale(button, 1, 1.1))
+      button.setOnMouseExited((event) => scale(button, 1.1, 1))
+    })
     view.btnRegularChart.setOnAction(event => {
       val newView = new RegularChartView()
       new RegularChartPresenter(newView)
@@ -19,6 +28,14 @@ class MenuPresenter(val view: MenuView) {
     })
   }
 
-  def updateView(): Unit = {
+  private def scale(button: Button, fromXY: Double, toXY: Double): Unit = {
+    val st = new ScaleTransition()
+    st.setNode(button)
+    st.setFromX(fromXY)
+    st.setFromY(fromXY)
+    st.setToX(toXY)
+    st.setToY(toXY)
+    st.setDuration(Duration.millis(50))
+    st.play
   }
 }
