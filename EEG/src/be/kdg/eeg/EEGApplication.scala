8800da3fceff8bc678df540720ef696a1cca3c78
Bryan <br.deridder@gmail.com>
2018-12-06 21:09:53 +0100

    added icon

--- EEG/src/be/kdg/eeg/EEGApplication.scala
+++ EEG/src/be/kdg/eeg/EEGApplication.scala
@@ -5,6 +5,7 @@ import be.kdg.eeg.view.MenuView
 import javafx.application.Application
 import javafx.stage.Stage
 import javafx.scene.Scene
+import javafx.scene.image.Image
 
 object EEGApplication {
   def main(args: Array[String]): Unit = {
@@ -17,6 +18,7 @@ class EEGApplication extends Application {
   private final val STAGE_MIN_WIDTH = 1024
   private final val STAGE_TITLE = "EEG Application"
   private final val STYLE_SHEET_PATH = "be/kdg/eeg/view/css/stylesheet.css"
+  private final val ICON = "be/kdg/eeg/view/img/icon.png"
 
   override def start(primaryStage: Stage): Unit = {
     val view = new MenuView()
@@ -25,6 +27,7 @@ class EEGApplication extends Application {
     val scene = new Scene(view)
     scene.getStylesheets.add(STYLE_SHEET_PATH)
 
+    primaryStage.getIcons.add(new Image(ICON))
     primaryStage.setTitle(STAGE_TITLE)
     primaryStage.setScene(scene)
     primaryStage.setMinHeight(STAGE_MIN_HEIGHT)
