1269d17ae8e369fe763fbe1d109953d7a35f6045
Bryan <br.deridder@gmail.com>
2018-12-07 11:15:23 +0100

    Added average line

--- EEG/src/be/kdg/eeg/presenter/SlidingWindowPresenter.scala
+++ EEG/src/be/kdg/eeg/presenter/SlidingWindowPresenter.scala
@@ -1,32 +1,30 @@
 package be.kdg.eeg.presenter
 
-import be.kdg.eeg.model.stimulus.{Stimulus, StimulusService, StimulusServiceStore}
-import be.kdg.eeg.view.{MenuView, RegularChartView, SlidingWindowView}
-import javafx.beans.value.{ChangeListener, ObservableValue}
+import be.kdg.eeg.model.stimulus.{StimulusService, StimulusServiceStore}
+import be.kdg.eeg.view.{MenuView, SlidingWindowView}
+import com.sun.javafx.charts.Legend
 import javafx.collections.FXCollections
-import javafx.scene.chart.{LineChart, XYChart}
+import javafx.scene.Cursor
+import javafx.scene.chart.XYChart
+import javafx.scene.control.Tooltip
+import javafx.util.Duration
 
 class SlidingWindowPresenter(val view: SlidingWindowView) {
-
+  private final val CHART_TOOLTIP_DELAY = new Duration(10)
   private val store = new StimulusServiceStore
 
   addEventHandlers()
   updateView()
 
   def addEventHandlers(): Unit = {
+    view.getBtnAddData.setOnAction(_ => updateChart())
     view.getBtnClear.setOnAction(_ => clearChart())
     view.getBtnBack.setOnAction(_ => {
       val newView = new MenuView()
       new MenuPresenter(newView)
       view.getScene.setRoot(newView)
     })
-    view.getComboBoxStimulus.valueProperty().addListener((_, _, newValue) => updateData(stimulus = newValue))
-    view.getComboBoxContactPoint.valueProperty().addListener((_, _, newValue) => updateData(contactPoint = newValue))
-    view.getComboBoxPersonInput.valueProperty().addListener((_, _, newValue) => {
-      updateView(newValue)
-      clearChart()
-      view.getChart.setTitle(newValue + "'s brain activity over time")
-    })
+    view.getComboBoxPersonInput.valueProperty().addListener((_, _, newValue) => updateView(newValue))
   }
 
   def updateView(name: String = null): Unit = {
@@ -46,7 +44,41 @@ class SlidingWindowPresenter(val view: SlidingWindowView) {
     view.getComboBoxStimulus.setItems(stimulusOptions)
   }
 
-  def updateChart(title: String, yValues: Vector[Double]) = {
+  /**
+    * Adds an onclick listener to the chart legend.
+    * When the legend is clicked the corresponding chart is hidden.
+    */
+  def enableHideOnClick(): Unit = {
+    view.getChart.getChildrenUnmodifiable.forEach {
+      case l: Legend =>
+        l.getItems.forEach(li => {
+          view.getChart.getData.filtered(s => s.getName.equals(li.getText))
+            .forEach(s => {
+              li.getSymbol.setCursor(Cursor.HAND)
+              li.getSymbol.setOnMouseClicked(_ => s.getNode.setVisible(!s.getNode.isVisible))
+            })
+        })
+      case _ =>
+    }
+  }
+
+  /**
+    * Adds tooltips to all datapoints in the chart
+    */
+  def addTooltips(): Unit = {
+    view.getChart.getData.forEach(s => {
+      s.getData.filtered(d => d.getNode.isVisible).forEach(d => {
+        val tooltip = new Tooltip("Activity: %s\nTime: %sms".format(
+          (math floor d.getYValue.floatValue() * 100) / 100, d.getXValue.toString))
+        tooltip.setShowDelay(CHART_TOOLTIP_DELAY)
+        Tooltip.install(d.getNode, tooltip)
+        d.getNode.setOnMouseEntered(_ => d.getNode.getStyleClass.add("hover-contact-point"))
+        d.getNode.setOnMouseExited(_ => d.getNode.getStyleClass.remove("hover-contact-point"))
+      })
+    })
+  }
+
+  def addDataToChart(title: String, yValues: Vector[Double]): Unit = {
     val series = new XYChart.Series[Number, Number]
     series.setName(title)
     yValues.indices.foreach(i => {
@@ -55,11 +87,53 @@ class SlidingWindowPresenter(val view: SlidingWindowView) {
     view.getChart.getData.add(series)
   }
 
-  def updateData(stimulus: String = view.getComboBoxStimulus.getValue,
-                 contactPoint: String = view.getComboBoxContactPoint.getValue): Unit = {
+  /**
+    * Checks if the data that is being added is already on the chart.
+    *
+    * @param title of the legend
+    * @return true of false
+    */
+  def dataAlreadyAdded(title: String): Boolean = {
+    view.getChart.getChildrenUnmodifiable.forEach {
+      case l: Legend =>
+        l.getItems.forEach(li => {
+          if (li.getText.equals(title)) return true
+        })
+      case _ =>
+    }
+    false
+  }
+
+  /**
+    * Adds and average line for a given contactpoint.
+    * It will only be added if the corresponding checkbox is checked.
+    *
+    * @param stimulus     given word
+    * @param contactPoint given point to calculate average for
+    */
+  def addAvgLine(stimulus: String, contactPoint: String): Unit = {
+    if (view.getCheckboxAvgLine.isSelected) {
+      val average = getModel.analyseTools.getAvgForContactPoints(stimulus, contactPoint)
+      val title = s"avg: ${view.getComboBoxPersonInput.getValue} - $stimulus: $contactPoint"
+      addDataToChart(title, Vector.fill(512)(average))
+    }
+  }
+
+  /**
+    * Updates the chart if both a stimulus and a contactpoint is present
+    */
+  def updateChart(): Unit = {
+    val stimulus: String = view.getComboBoxStimulus.getValue
+    val contactPoint: String = view.getComboBoxContactPoint.getValue
     if (stimulus != null && contactPoint != null) {
       val data = getModel.getContactPointValuesForStimulus(stimulus, contactPoint)
-      updateChart(stimulus + ": " + contactPoint, data)
+      val title = s"${view.getComboBoxPersonInput.getValue} - $stimulus: $contactPoint"
+      if (!dataAlreadyAdded(title)) {
+        addDataToChart(title, data)
+        addAvgLine(stimulus, contactPoint)
+        enableHideOnClick()
+        addTooltips()
+      }
     }
   }
 
--- EEG/src/be/kdg/eeg/view/SlidingWindowView.scala
+++ EEG/src/be/kdg/eeg/view/SlidingWindowView.scala
@@ -1,11 +1,13 @@
 package be.kdg.eeg.view
 
-import javafx.geometry.{Insets, Pos}
 import javafx.scene.chart.{LineChart, NumberAxis}
-import javafx.scene.control.{Button, ComboBox, Label}
+import javafx.scene.control._
 import javafx.scene.layout.{BorderPane, HBox, VBox}
+import javafx.util.Duration
 
 class SlidingWindowView extends BorderPane {
+  private final val MAX_TIME: Double = 512
+  private final val TICK_UNIT: Double = 50
   private final val DATA = "Data:"
   private final val STIMULUS = "Stimulus:"
   private final val CONTACT_POINT = "Contact point:"
@@ -13,6 +15,12 @@ class SlidingWindowView extends BorderPane {
   private final val Y_AXIS = "Activity"
   private final val CLEAR_CHART = "Clear"
   private final val BACK = "Back"
+  private final val ADD_DATA = "Add data"
+  private final val BACK_TOOLTIP = "Back to menu"
+  private final val CLEAR_TOOLTIP = "Clear the chart"
+  private final val ADD_DATA_TOOLIP = "Add data to the chart"
+  private final val CHART_TITLE = "Brain activity over time"
+  private final val BUTTON_TOOLTIP_DELAY = new Duration(500)
 
   //Nodes
   private val comboBoxStimulus = new ComboBox[String]
@@ -20,9 +28,20 @@ class SlidingWindowView extends BorderPane {
   private val comboBoxPersonInput = new ComboBox[String]
   private val btnClear = new Button(CLEAR_CHART)
   private val btnBack = new Button(BACK)
+  private val btnAddData = new Button(ADD_DATA)
+  private val cbxAddAvgLine = new CheckBox("Average line")
+  private val tooltipBack = new Tooltip(BACK_TOOLTIP)
+  private val tooltipClear = new Tooltip(CLEAR_TOOLTIP)
+  private val tooltipAddData = new Tooltip(ADD_DATA_TOOLIP)
+  tooltipBack.setShowDelay(BUTTON_TOOLTIP_DELAY)
+  tooltipClear.setShowDelay(BUTTON_TOOLTIP_DELAY)
+  tooltipAddData.setShowDelay(BUTTON_TOOLTIP_DELAY)
   private val chart: LineChart[Number, Number] = {
     val xAxis = new NumberAxis
     xAxis.setLabel(X_AXIS)
+    xAxis.setAutoRanging(false)
+    xAxis.setTickUnit(TICK_UNIT)
+    xAxis.setUpperBound(MAX_TIME)
     val yAxis = new NumberAxis
     yAxis.setForceZeroInRange(false)
     yAxis.setLabel(Y_AXIS)
@@ -36,20 +55,23 @@ class SlidingWindowView extends BorderPane {
     val vboxData = new VBox(new Label(DATA), comboBoxPersonInput)
     val vboxStimulus = new VBox(new Label(STIMULUS), comboBoxStimulus)
     val vboxContact = new VBox(new Label(CONTACT_POINT), comboBoxContactPoint)
-    val hbox = new HBox(vboxData, vboxStimulus, vboxContact)
+    val hbox = new HBox(vboxData, vboxStimulus, vboxContact, cbxAddAvgLine, btnAddData)
     hbox.getStyleClass.add("toolbar")
-    chart.setCreateSymbols(false)
     this.setCenter(chart)
     val bottomLeftPane = new BorderPane()
     val bottomRightPane = new BorderPane()
+    btnBack.setTooltip(tooltipBack)
+    btnClear.setTooltip(tooltipClear)
+    btnAddData.setTooltip(tooltipAddData)
     bottomLeftPane.setBottom(btnBack)
     bottomRightPane.setBottom(btnClear)
-    bottomLeftPane.getStyleClass.add("bottomLeftToolbar")
-    bottomRightPane.getStyleClass.add("bottomRightToolbar")
+    bottomLeftPane.getStyleClass.add("bottom-left-toolbar")
+    bottomRightPane.getStyleClass.add("bottom-right-toolbar")
     val bottomPane = new BorderPane(hbox)
     bottomPane.setLeft(bottomLeftPane)
     bottomPane.setRight(bottomRightPane)
     this.setBottom(bottomPane)
+    chart.setTitle(CHART_TITLE)
   }
 
   def getComboBoxStimulus: ComboBox[String] = comboBoxStimulus
@@ -61,6 +83,10 @@ class SlidingWindowView extends BorderPane {
   def getBtnClear: Button = btnClear
 
   def getBtnBack: Button = btnBack
+
+  def getBtnAddData: Button = btnAddData
+
+  def getCheckboxAvgLine: CheckBox = cbxAddAvgLine
 
   def getChart: LineChart[Number, Number] = chart
 }
