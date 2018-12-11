b20b3e49f4e56fe29e144848095ec4c8c8ecacbe
Jarne <jarne.vanaerde@hotmail.com>
2018-12-11 14:59:13 +0100

    Update NumberField.scala

--- EEG/src/be/kdg/eeg/view/util/NumberField.scala
+++ EEG/src/be/kdg/eeg/view/util/NumberField.scala
@@ -5,7 +5,7 @@ import javafx.scene.control.TextField
 /**
   * Custom TextField class that only accepts numbers with a max length of 1.
   */
-class NumberField(lowerBound: Int, upperBound: Int) extends TextField {
+final class NumberField(lowerBound: Int, upperBound: Int) extends TextField {
   this.textProperty.addListener((_, oldValue, newValue) => {
     val allDigit = newValue forall Character.isDigit
     if (!newValue.isEmpty && (!allDigit || newValue.toInt > upperBound || newValue.toInt <= lowerBound)) {
